5c1f94c50af8b036c353da1ccc36904f132445e1
Gabriel Pivaro <gabrconc@gmail.com>
2020-10-29 01:19:56 -0500

    Notebook working

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
 
-# From the separate python file in this directory, we'll import the code that is used to scrape craigslist
+# From the separate python file in this directory, we'll import the code that is used to scrape the pages
 from scrape_mars import scrape
 
+app = Flask(__name__)
 
-# Next, create a route called /scrape that will import your scrape_mars.py script and call your scrape function.
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_mission_scraping"
+mongo = PyMongo(app)
 
-# Store the return value in Mongo as a Python dictionary.
-# Create a root route / that will query your Mongo database and pass the mars data into an HTML template to display the data.
+# identify the collection and drop any existing data for this demonstration
+mars_data = mongo.db.mars_data
+mars_data.drop()
 
+data = [{"Nasa title": "Change This", "Nasa par": "Test Test Test"}]
+
+
+# Render the index.html page with any craigslist listings in our database.
+# If there are no listings, the table will be empty.
+@app.route("/")
+def index():
+    mars_info = list(mars_data.find())
+    # return render_template("index.html", listing_results=listing_results)
+    return mars_info
+    # return "Hello World"
+
+
+# This route will trigger the webscraping, but it will then send us back to the index route to render the results
+@app.route("/scrape")
+def scraper():
+
+    # scrape_craigslist.scrape() is a custom function that we've defined in the scrape_mars.py file within this directory
+    # scraped_data = scrape()
+    # list_data = [scraped_data]
+    # print("-----" * 35)
+    # print(results)
+    mars_data.drop()
+    mars_data.insert_many(data)
+
+    # Use Flask's redirect function to send us to a different route once this task has completed.
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -8,155 +8,160 @@ import pandas as pd
 
 def scrape():
 
-    scrape_dict = {}
+    scraped_data = {}
 
-    ### Setup Splinter
-    # identify location of chromedriver and store it as a variable
-    # driverPath = !which chromedriver
-    driverPath = ["/usr/local/bin/chromedriver"]
+    # ### Setup Splinter
+    # # identify location of chromedriver and store it as a variable
+    # # driverPath = !which chromedriver
+    # driverPath = ["/usr/local/bin/chromedriver"]
 
-    # Setup configuration variables to enable Splinter to interact with browser
-    executable_path = {"executable_path": driverPath[0]}
-    browser = Browser("chrome", **executable_path, headless=False)
+    # # Setup configuration variables to enable Splinter to interact with browser
+    # executable_path = {"executable_path": driverPath[0]}
+    # browser = Browser("chrome", **executable_path, headless=False)
 
-    ################################ NASA Mars News ###################################
-    ###################################################################################
-    # URL of page to be scraped
-    # url_nasa = "https://mars.nasa.gov/news/"
-    url_nasa = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+    # ################################ NASA Mars News ###################################
+    # ###################################################################################
+    # # URL of page to be scraped
+    # # url_nasa = "https://mars.nasa.gov/news/"
+    # url_nasa = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
 
-    #### Use splinter to inform the browser to visit the page
-    # Use the browser to visit the url
-    browser.visit(url_nasa)
+    # #### Use splinter to inform the browser to visit the page
+    # # Use the browser to visit the url
+    # browser.visit(url_nasa)
 
-    # Use beatifulsoup to scrap the page rendered by the browser
-    html_nasa = browser.html
-    soup = BeautifulSoup(html_nasa, "html.parser")
+    # # Use beatifulsoup to scrap the page rendered by the browser
+    # html_nasa = browser.html
+    # soup = BeautifulSoup(html_nasa, "html.parser")
 
-    # Print the li that contatins the first headline
-    # results = soup.find("div", class_="content_title")
-    results = soup.find_all("div", class_="content_title")
+    # # # Print the li that contatins the first headline
+    # # results = soup.find("div", class_="content_title")
+    # # # results = soup.find_all("div", class_="content_title")
 
-    # Assign the text to variables that you can reference later
-    # news_title = results.a.text
-    news_title = results[1].text
-    print(f"Title: {news_title}\n")
+    # # # Assign the text to variables that you can reference later
+    # # # news_title = results.a.text
+    # # news_title = results[1].text
+    # # print(f"Title: {news_title}\n")
 
-    results = soup.find('li', class_="slide")
-    news_p = results.find("div", class_="article_teaser_body").text
-    print(f"{news_p}")
+    # # results = soup.find("li", class_="slide")
+    # # news_p = results.find("div", class_="article_teaser_body").text
+    # # print(f"{news_p}")
 
-    scrape_dict["Nasa title"] = news_title
-    scrape_dict["Nasa par"] = news_p
+    # # scraped_data["Nasa title"] = news_title
+    # # scraped_data["Nasa par"] = news_p
 
-    ##################### JPL Mars Space Images - Featured Image ######################
-    ###################################################################################
-    # URL for JPL Nasa websit
-    url_jpl = "https://www.jpl.nasa.gov"
+    scraped_data["Nasa title"] = "Change This"
+    scraped_data["Nasa par"] = "Test Test Test"
 
-    # The url for JPL Featured Space Image
-    space_images = "/spaceimages/?search=&category=Mars"
+    # ##################### JPL Mars Space Images - Featured Image ######################
+    # ###################################################################################
+    # # URL for JPL Nasa websit
+    # url_jpl = "https://www.jpl.nasa.gov"
 
-    # Full url
-    url_jpl_space_images = f"{url_jpl}{space_images}"
+    # # The url for JPL Featured Space Image
+    # space_images = "/spaceimages/?search=&category=Mars"
 
-    # Use the browser to visit the url
-    browser.visit(url_jpl_space_images)
+    # # Full url
+    # url_jpl_space_images = f"{url_jpl}{space_images}"
 
-    # Use beatifulsoup to scrap the page rendered by the browser
-    html_jpl = browser.html
-    soup = BeautifulSoup(html_jpl, "html.parser")
+    # # Use the browser to visit the url
+    # browser.visit(url_jpl_space_images)
 
-    # Featured image is in the div class="carousel_container"
-    results = soup.find("div", class_="carousel_container")
+    # # Use beatifulsoup to scrap the page rendered by the browser
+    # html_jpl = browser.html
+    # soup = BeautifulSoup(html_jpl, "html.parser")
 
-    # Find the article
-    article = results.find("article")
+    # # Featured image is in the div class="carousel_container"
+    # results = soup.find("div", class_="carousel_container")
 
-    # Grab the style string and split
-    style = article["style"].split("(")
+    # # Find the article
+    # article = results.find("article")
 
-    # Retrieve the url strig location
-    image_location = style[1].split(")")[0][1:-1]
+    # # Grab the style string and split
+    # style = article["style"].split("(")
 
-    # Compose the full url of the image
-    featured_image_url = f"{url_jpl}{image_location}"
-    print(featured_image_url)
+    # # Retrieve the url strig location
+    # image_location = style[1].split(")")[0][1:-1]
 
-    scrape_dict["JPL image"] = featured_image_url
+    # # Compose the full url of the image
+    # featured_image_url = f"{url_jpl}{image_location}"
+    # print(featured_image_url)
 
-    ############################ Mars Facts ###########################################
-    ###################################################################################
-    # URL
-    url_mars_facts = "https://space-facts.com/mars/"
+    # scraped_data["JPL image"] = featured_image_url
 
-    # Use Pandas to automatically scrape any tabular data from a page.
-    tables = pd.read_html(url_mars_facts)
+    # ############################ Mars Facts ###########################################
+    # ###################################################################################
+    # # URL
+    # url_mars_facts = "https://space-facts.com/mars/"
 
-    # Select the intended table
-    table_facts = tables[0]
-    html_table = table_facts.to_html()
-    print(html_table)
-    table_facts.to_html("table.html")
+    # # Use Pandas to automatically scrape any tabular data from a page.
+    # tables = pd.read_html(url_mars_facts)
 
-    scrape_dict["Mars table"] = html_table
+    # # Select the intended table
+    # table_facts = tables[0]
+    # html_table = table_facts.to_html()
+    # print(html_table)
+    # table_facts.to_html("table.html")
 
-    ############################# Mars Hemispheres ####################################
-    ###################################################################################
-    # URL
-    url_mars_hemispheres = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    # scraped_data["Mars table"] = html_table
 
-    # Use the browser to visit the url
-    browser.visit(url_mars_hemispheres)
+    # ############################# Mars Hemispheres ####################################
+    # ###################################################################################
+    # # URL
+    # url_mars_hemispheres = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
 
-    # Splinter can capture a page's underlying html and use pass it to BeautifulSoup to allow us to scrape the content
-    html = browser.html
-    soup = BeautifulSoup(html, "html.parser")
+    # # Use the browser to visit the url
+    # browser.visit(url_mars_hemispheres)
 
-    # By analyzing the page we can find that the images are in a div class='description'
-    results = soup.find_all("div", class_="description")
+    # # Splinter can capture a page's underlying html and use pass it to BeautifulSoup to allow us to scrape the content
+    # html = browser.html
+    # soup = BeautifulSoup(html, "html.parser")
 
-    # Create a list with the name of the hemispheres
-    list_hemispheres = []
-    for i in range(len(results)):
-        list_hemispheres.append(results[i].a.h3.text)
+    # # By analyzing the page we can find that the images are in a div class='description'
+    # results = soup.find_all("div", class_="description")
 
-    print(list_hemispheres)
-    hemisphere_image_urls = []
+    # # Create a list with the name of the hemispheres
+    # list_hemispheres = []
+    # for i in range(len(results)):
+    #     list_hemispheres.append(results[i].a.h3.text)
 
-    # Create a list of dictionaries for each hemisphere
-    for i in range(len(list_hemispheres)):
+    # print(list_hemispheres)
+    # hemisphere_image_urls = []
 
-        # Use the browser to visit the url
-        browser.click_link_by_partial_text(list_hemispheres[i])
+    # # Create a list of dictionaries for each hemisphere
+    # for i in range(len(list_hemispheres)):
 
-        # Splinter can capture a page's underlying html and use pass it to BeautifulSoup to allow us to scrape the content
-        html = browser.html
-        soup = BeautifulSoup(html, "html.parser")
+    #     # Use the browser to visit the url
+    #     browser.click_link_by_partial_text(list_hemispheres[i])
 
-        # By analyzing the page we can find that the images link are in a li
-        results_new = soup.find_all("li")
+    #     # Splinter can capture a page's underlying html and use pass it to BeautifulSoup to allow us to scrape the content
+    #     html = browser.html
+    #     soup = BeautifulSoup(html, "html.parser")
 
-        # Append the dictionary with the image url string and the hemisphere title to a list.
-        for n in range(len(results_new)):
-            if results_new[n].a.text == "Original":
-                hemisphere_image_urls.append(
-                    {"title": list_hemispheres[i], "img_url": results_new[1].a["href"]}
-                )
+    #     # By analyzing the page we can find that the images link are in a li
+    #     results_new = soup.find_all("li")
 
-        # Use the browser to visit the url
-        browser.visit(url_mars_hemispheres)
+    #     # Append the dictionary with the image url string and the hemisphere title to a list.
+    #     for n in range(len(results_new)):
+    #         if results_new[n].a.text == "Original":
+    #             hemisphere_image_urls.append(
+    #                 {"title": list_hemispheres[i], "img_url": results_new[1].a["href"]}
+    #             )
 
-    print(hemisphere_image_urls)
+    #     # Use the browser to visit the url
+    #     browser.visit(url_mars_hemispheres)
 
-    scrape_dict["Mars Hemispheres"] = hemisphere_image_urls
+    # print(hemisphere_image_urls)
+
+    # scraped_data["Mars Hemispheres"] = hemisphere_image_urls
 
     # When you’ve finished testing, close your browser using browser.quit:
-    browser.quit()
+    # browser.quit()
 
-    return scrape_dict
+    return scraped_data
 
 
-results = scrape()
-
-print(results)
+# data = scrape()
+# print()
+# print()
+# print("-----------")
+# print(data)
